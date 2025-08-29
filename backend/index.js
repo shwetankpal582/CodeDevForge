aaa6c05458d69d4fc70b98e28fcf5b9ab58bbd84
Shwetank Pal <shwetankpal582@gmail.com>
2025-08-29 19:54:44 +0530

    Updated ENV

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
-const dotenv = require('dotenv');
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
