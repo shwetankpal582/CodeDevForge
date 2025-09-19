b20bf6343ac7809ef7453a5a4f5a046bbc7eb18b
Shwetank Pal <shwetankpal582@gmail.com>
2025-09-20 02:04:59 +0530

    Remove secrets and use environment variables

--- backend/index.js
+++ backend/index.js
@@ -53,3 +53,8 @@ app.use(cors({
   ],
   credentials: true
 }));
+
+const CLIENT_URL = process.env.CLIENT_URL;
+const JWT_SECRET = process.env.JWT_SECRET;
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
